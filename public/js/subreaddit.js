d0ffc85c52d800e6f0efe11d681db782cfe64182
TYRPRO <www.yongrui@gmail.com>
2021-11-15 21:03:12 +0800

    added frontend removal of posts if user is subreaddit owner or moderator.

--- public/js/subreaddit.js
+++ public/js/subreaddit.js
@@ -162,6 +162,8 @@ $(document).ready(function () {
             data = sortedData;
 
             for (var i = 0; i < data.length; i++) {
+                console.log("Number of posts: " + data.length)
+                console.log(JSON.stringify(data[i]));
                 var copyStr = data[i].Subreaddit.subreaddit_name + "/" + data[i].post_id;
                 // Calculates Time
                 var date = new Date(data[i].created_at);
@@ -187,6 +189,32 @@ $(document).ready(function () {
                     post_date_output = `${weeks_between_dates} weeks ago`
                 }
 
+                var pinnedStr = "";
+                if(data[i].pinned == 1){
+                    pinnedStr =  `<p class="fw-light text-secondary mx-1">•</p>
+                                <p class="text-secondary">Pinned By Moderators</p>
+                    `
+                }
+
+                var report_delete = "";
+                if (owner||moderator){
+                    report_delete = `
+                    <button style="border-width : 0px; background-color:white;" type="button" class="delete" id="delete_${data[i].post_id}_${data[i].Subreaddit.subreaddit_id}">
+                        <div class="d-flex flex-row text-secondary me-4">
+                            <span class="material-icons md-24 mx-1">outlined_flag</span>
+                            <p class="mb-0 fw-bold fs-6">Delete</p>
+                        </div>
+                    </button>`
+                }
+                else{
+                    report_delete = `
+                    <button style="border-width : 0px; background-color:white;" type="button" onclick="report(${data[i].post_id})" id="report">
+                        <div class="d-flex flex-row text-secondary me-4">
+                            <span class="material-icons md-24 mx-1">outlined_flag</span>
+                            <p class="mb-0 fw-bold fs-6">Report</p>
+                        </div>
+                    </button>`
+                }
 
                 var append_str = "";
                 append_str = `<div class="post rounded mb-2" id="post_${data[i].post_id}">
@@ -207,46 +235,34 @@ $(document).ready(function () {
                         <p class="d-inline text-secondary clickable-link" id="post_${data[i].User.username}_user"> u/<a>${data[i].User.username}</a></p>
                         <p class="fw-light text-secondary mx-1">•</p>
                         <p class="text-secondary" id="post_${data[i].post_id}_time">${post_date_output}</p>
-            `
-            //indicator for pins
-            if(data[i].pinned == 1){
-                append_str += `<p class="fw-light text-secondary mx-1">•</p>
-                            <p class="text-secondary">Pinned By Moderators</p>
-                `
-            }
-            
-            append_str +=`</div>
-                    <a style="text-decoration:none" href="/r/${data[i].Subreaddit.subreaddit_name}/${data[i].post_id}">
-                    <h5 style="color : black;" id="post_${data[i].post_id}_content">${data[i].title}</h5>
-                    </a>
-                    <p>${data[i].content}<p>
-                    <div id="post_media${data[i].post_id}" class="d-flex flex-row justify-content-center bg-dark"> ${addImage(data[i].post_id)} </div>
-                    <div class="toolbar d-flex flex-row align-items-center mt-2">
-                            <div class="d-flex flex-row text-secondary me-4 p-1 rounded hoverable">
-                                <span class="material-icons md-24 ms-0 me-1">chat_bubble_outline</span>
-                                <p class="mb-0 fw-bold me-1" id="comment_total"></p>
-                                <p class="mb-0 fw-bold fs-6">Comments</p>
-                            </div>
-                            <div class="d-flex flex-row text-secondary me-4 p-1 rounded hoverable share" id="${copyStr}">
-                                <span class="material-icons md-24 ms-0 mx-1">share</span>
-                                <p class="mb-0 fw-bold fs-6">Share</p>
-                            </div>
-                            <div class="save d-flex flex-row text-secondary me-4 p-1 rounded hoverable" id="post_bookmark_${data[i].post_id}">
-                                <span class="material-icons ms-0">bookmark</span>
-                                <p class="mb-0 fw-bold fs-6">Unsave</p>
-                            </div>
-                            <button style="border-width : 0px; background-color:white;" type="button" onclick="report(${data[i].post_id})" id="report">
-                                <div class="d-flex flex-row text-secondary me-4">
-                                    <span class="material-icons md-24 mx-1">outlined_flag</span>
-                                    <p class="mb-0 fw-bold fs-6">Report</p>
-                                </div>
-                            </button>
+                        ${pinnedStr}
                     </div>
+                        <a style="text-decoration:none" href="/r/${data[i].Subreaddit.subreaddit_name}/${data[i].post_id}">
+                        <h5 style="color : black;" id="post_${data[i].post_id}_content">${data[i].title}</h5>
+                        </a>
+                        <p>${data[i].content}<p>
+                        <div id="post_media${data[i].post_id}" class="d-flex flex-row justify-content-center bg-dark"> ${addImage(data[i].post_id)} </div>
+                        <div class="toolbar d-flex flex-row align-items-center mt-2">
+                                <div class="d-flex flex-row text-secondary me-4 p-1 rounded hoverable">
+                                    <span class="material-icons md-24 ms-0 me-1">chat_bubble_outline</span>
+                                    <p class="mb-0 fw-bold me-1" id="comment_total"></p>
+                                    <p class="mb-0 fw-bold fs-6">Comments</p>
+                                </div>
+                                <div class="d-flex flex-row text-secondary me-4 p-1 rounded hoverable share" id="${copyStr}">
+                                    <span class="material-icons md-24 ms-0 mx-1">share</span>
+                                    <p class="mb-0 fw-bold fs-6">Share</p>
+                                </div>
+                                <div class="save d-flex flex-row text-secondary me-4 p-1 rounded hoverable" id="post_bookmark_${data[i].post_id}">
+                                    <span class="material-icons ms-0">bookmark</span>
+                                    <p class="mb-0 fw-bold fs-6">Unsave</p>
+                                </div>
+                                ${report_delete}
+                        </div>
 
-                </div>
-                
-                </div>
-                </div>`
+                    </div>
+                    
+                    </div>
+                    </div>`
 
 
                 $('#post_div').append(append_str)
@@ -254,27 +270,10 @@ $(document).ready(function () {
             
             if (owner || moderator){
                 $(`#post_${data[i].post_id}`).append(`
-                <div class="pin" id="${data[i].post_id}_${data[i].Subreaddit.subreaddit_id}">
+                <div class="pin" id="pin_${data[i].post_id}_${data[i].Subreaddit.subreaddit_id}">
                     <span class="material-icons md-24 ms-0 mx-1">push_pin</span>
                 </div>  
             `)
-            }
-
-      
-            // Block of code shows user's upvotes and downvotes on posts
-            // temp user_id
-            var user_id = 2;
-            if (user_id) {
-                var data = getUsersVotes(pathname, user_id);
-                for(var i = 0 ; i<data.length; i++) {
-                    var upvote_button = $(`#post_${data[i].fk_post_id}_upvote`);
-                    var downvote_button = $(`#post_${data[i].fk_post_id}_downvote`);
-                    if(data[i].vote_type == true) {
-                        upvote_button.addClass('upvoted');
-                    }else {
-                        downvote_button.addClass('downvoted');
-                    }
-                }
             }
 
 
@@ -481,6 +480,27 @@ $(document).ready(function () {
                 pin(pin_id);
             })
 
+            $('.delete').on('click', function (e) {
+                e.stopPropagation();
+                var delete_id = $(this).attr('id');
+                deletePost(delete_id);
+            })
+
+        }
+        // Block of code shows user's upvotes and downvotes on posts
+        // temp user_id
+        var user_id = 2;
+        if (user_id) {
+            var data = getUsersVotes(pathname, user_id);
+            for(var i = 0 ; i<data.length; i++) {
+                var upvote_button = $(`#post_${data[i].fk_post_id}_upvote`);
+                var downvote_button = $(`#post_${data[i].fk_post_id}_downvote`);
+                if(data[i].vote_type == true) {
+                    upvote_button.addClass('upvoted');
+                }else {
+                    downvote_button.addClass('downvoted');
+                }
+            }
         }
     },
     error: function (xhr, status, error) {
@@ -557,8 +577,8 @@ function copy(copyStr) {
 
 function pin(post_subreaddit_id){
     var post_subreaddit_id_arr = post_subreaddit_id.split('_');
-    var post_id = post_subreaddit_id_arr[0]
-    var fk_subreaddit_id = post_subreaddit_id_arr[1]
+    var post_id = post_subreaddit_id_arr[1]
+    var fk_subreaddit_id = post_subreaddit_id_arr[2]
     var data = JSON.stringify({post_id:post_id,fk_subreaddit_id:fk_subreaddit_id});
     var token = localStorage.getItem("token");
     $.ajax({
@@ -586,4 +606,25 @@ function copy(copyStr) {
 
 function report(post_id) {
     window.location.assign(baseUrl[1] + '/report.html?post_id=' + post_id);
+}
+
+function deletePost(post_subreaddit_id){
+    var post_subreaddit_id_arr = post_subreaddit_id.split('_');
+    var post_id = post_subreaddit_id_arr[1]
+    var fk_subreaddit_id = post_subreaddit_id_arr[2]
+    var data = JSON.stringify({post_id:post_id,fk_subreaddit_id:fk_subreaddit_id});
+    var token = localStorage.getItem("token");
+    $.ajax({
+        url: `${baseUrl[0]}/post`,
+        method: 'DELETE',
+        contentType: "application/json; charset=utf-8",
+        headers:{'authorization': "Bearer " + token},
+        data: data,
+        success: function (data, status, xhr) {
+            window.location.reload()
+        },
+        error: function (xhr, status, error) {
+            alert("Error deleting post")
+        }
+    });
 }
