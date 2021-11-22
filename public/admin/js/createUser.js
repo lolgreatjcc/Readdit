4c2083c455043b3b933c7bddc77468d432a3efab
Klyntar5 <adamhowzk@gmail.com>
2021-11-22 19:16:47 +0800

    Fixed Auth

--- public/admin/js/createUser.js
+++ public/admin/js/createUser.js
@@ -36,7 +36,7 @@ function addUser(username, email, profile_pic, password, two_fa, image) {
                 console.log(error.response.status);
                 notifier.alert(error.response.data.message)
             }
-
+            
         })
 }
 
@@ -47,13 +47,20 @@ $(document).ready(function () {
         // userData = userData.slice(1,-1);
 
         var userJsonData = JSON.parse(userData);
-        var role = userJsonData.fk_user_type_id;
 
-        var tmpToken = localStorage.getItem('token');
-
-        if (role != 2) {
-            window.location.assign(`${baseUrl[1]}/home.html`);
-        }
+        $.ajax({
+            headers: { 'authorization': 'Bearer ' + token },
+            url: `${baseUrl[0]}/verify`,
+            type: 'GET',
+            contentType: "application/json; charset=utf-8",
+            dataType: 'json',
+            success: function (data, textStatus, xhr) {
+                console.log(data);
+            },
+            error: function (xhr, textStatus, errorThrown) {
+                window.location.assign(`${baseUrl[1]}/home.html`);
+            }
+        });
     } catch (error) {
         window.location.assign(`${baseUrl[1]}/login.html`);
     }
