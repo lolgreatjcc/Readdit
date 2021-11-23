c1e29d0fe2bc211829173841b1740cbd24ef9424
Klyntar5 <adamhowzk@gmail.com>
2021-11-23 22:08:23 +0800

    Final Fix

--- controller/save.js
+++ controller/save.js
@@ -6,11 +6,12 @@ const verify = require('./verify')
 // save a post
 router.post('/post',verify.verifySameUserId, function (req,res) {
     var post_id = req.body.post_id;
-    var user_id = req.body.token_user_id;
+    var user_id = req.body.user_id;
     post.savePost(post_id, user_id, function (result,err) {
         if(!err) {
             res.status(200).send(result);
         } else {
+            console.log(err);
             res.status(500).send({'message':"Error saving post."});
         }
     })
